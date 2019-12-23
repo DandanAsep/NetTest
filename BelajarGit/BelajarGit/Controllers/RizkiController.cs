eda127d57655cd413ff964bb6d94bdebf21f50b8
rizki0212 <rizki021296@gmail.com>
2019-12-23 14:39:50 +0700

    membuat RizkiController.cs

---
+++ BelajarGit/BelajarGit/Controllers/RizkiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BelajarGit.Controllers
+{
+    public class RizkiController : Controller
+    {
+        //
+        // GET: /Rizki/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
