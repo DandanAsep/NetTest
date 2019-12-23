a5cb4b3a3d56fdecb0453d8ab115b73ca3e0a8f1
mutiasha <shafirach98@gmail.com>
2019-12-23 14:17:39 +0700

    commit satu

---
+++ BelajarGit/BelajarGit/Controllers/BelajarGitController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BelajarGit.Controllers
+{
+    public class BelajarGitController : Controller
+    {
+        //
+        // GET: /BelajarGit/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
